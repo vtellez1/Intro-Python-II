82234bc4053ccfb2162e195dee7c7c6de8f9b5c5
Vanessa <vanessatellez45@gmail.com>
2020-04-08 18:57:35 -0400

    day 1 MVP met

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -49,3 +50,71 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+player1 = Player('Sally', room['outside'],
+                 room['outside'].description)
+print('Hello ' + str(player1) + ' Please make your first move.')
+
+moves = (input("[n] North  [s] South  [e] East [w] West [q] Quit: "))
+
+
+while not moves == "q":
+
+    # if player is OUTSIDE
+    if player1.room == room['outside']:
+        # if player chooses north
+        if moves == "n":
+            player1.room = room['outside'].n_to
+            print(player1)
+        else:
+            print('Sorry, cannot move that direction. Try again.')
+
+     # if player is in FOYER
+    elif player1.room == room['foyer']:
+        # if player chooses south
+        if moves == "s":
+            player1.room = room['foyer'].s_to
+            print(player1)
+        # if player chooses north
+        elif moves == "n":
+            player1.room = room['foyer'].n_to
+            print(player1)
+        # if player chooses east
+        elif moves == "e":
+            player1.room = room['foyer'].e_to
+            print(player1)
+        else:
+            print('Sorry, cannot move that direction. Try again.')
+
+     # if player is in OVERLOOK
+    elif player1.room == room['overlook']:
+        # if player chooses south
+        if moves == "s":
+            player1.room = room['overlook'].s_to
+            print(player1)
+        else:
+            print('Sorry, cannot move that direction. Try again.')
+
+    # if player is in NARROW
+    elif player1.room == room['narrow']:
+        # if player chooses north
+        if moves == "n":
+            player1.room = room['narrow'].n_to
+            print(player1)
+        # if player chooses west
+        elif moves == "w":
+            player1.room = room['narrow'].w_to
+            print(player1)
+        else:
+            print('Sorry, cannot move that direction. Try again.')
+
+    # if player is in TREASURE
+    elif player1.room == room['treasure']:
+        # if player chooses south
+        if moves == "s":
+            player1.room = room['treasure'].s_to
+            print(player1)
+        else:
+            print('Sorry, cannot move that direction. Try again.')
+
+    print("Please make your next move to continue...")
+    moves = (input("[n] North  [s] South  [e] East [w] West [q] Quit: "))
--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
+
+class Player(Room):
+    def __init__(self, name, room, description):
+        super().__init__(room, description)
+        self.name = name
+
+    def __str__(self):
+        return 'Player: %s, your current room: %s. Room description: %s' % (self.name, self.room.room, self.description)
